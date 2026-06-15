3a8250c2eeaec8f2bdb18e2dfe8b777c0198dbd3
safaa abdelkhalek <safaa3bdelkhaleq@gmail.com>
2026-06-15 14:20:39 +0300

    -

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://aya-khaled-portfolio-gamma.vercel.app'
+  // apiUrl: 'https://aya-khaled-portfolio-gamma.vercel.app'
+  apiUrl:'http://localhost:5000'
 };
